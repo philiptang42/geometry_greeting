ec24bea3b0cfc9d850c2435c2a1bec4334ae8be7
philiptang42 <philiptang42@gmail.com>
2015-12-08 19:26:06 -0500

    part 2 solution using rand(1..4)

--- geometry_greeting.rb
+++ geometry_greeting.rb
@@ -1,3 +1,16 @@
 def greet(name)
+  random = rand(1..4)
+  if random == 1
    puts "Hi #{name}!"
+  elsif random == 2
+   puts "Yo #{name}"
+  elsif random == 3
+   puts "Hey #{name}"
+  else
+   puts "What's up #{name}?"
+ end
 end
+
+puts "Whats your name?"
+
+greet(gets.chomp)
