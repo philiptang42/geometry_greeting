601a040027caa9651ad19fa8fa2778e2476c4d04
philiptang42 <philiptang42@gmail.com>
2015-12-09 19:51:26 -0500

    step 3 progression

--- geometry_greeting.rb
+++ geometry_greeting.rb
@@ -1,13 +1,17 @@
 def greet(name, language)
   random = rand(1..4)
-  if random == 1
-   puts "Hi #{name}!"
-  elsif random == 2
-   puts "Yo #{name}"
-  elsif random == 3
-   puts "Hey #{name}"
-  else
-   puts "What's up #{name}?"
+  if language != "Spanish" || language != "Italian" || language != "French"
+    if random == 1
+     puts "Hi #{name}!"
+    elsif random == 2
+     puts "Yo #{name}"
+    elsif random == 3
+     puts "Hey #{name}"
+    else
+     puts "What's up #{name}?"
+   end
+ else
+   puts "lol"
  end
 end
 
